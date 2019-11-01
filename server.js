ad882766c9926c409aaf113a91052d416cee2050
Tapiwanashe Mlangeni <tmlangeni@gmail..com>
2019-11-01 11:47:52 +0200

    Configured dependencies

--- server.js
+++ server.js
@@ -5,4 +5,27 @@ const mongoose = require("mongoose");
 
 const userRoutes = require("./routes/account")
 
+const config = require("./util/config");
+
+const app = express();
+
+
+mongoose.connect(config.database, { useNewUrlParser: true }, err => {
+	if (err) {
+	  	console.log("Connection Error \n" + err);
+	    } else {
+		console.log("Successfully connected to the database");
+	    }
+});
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+app.use("/api/accounts", userRoutes);
+
+app.listen(config.port, err => {
+	  console.log("Application server running on port " + config.port);
+});
 
