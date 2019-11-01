54a60f5e770176bf4bfe6370668ad02cf426b980
Tapiwanashe Mlangeni <tmlangeni@gmail..com>
2019-11-01 09:39:29 +0200

    Initial commit for the project

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/account")
+
+
